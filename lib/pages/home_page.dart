8802ab39fb18bdcb0a66bb0559e3a7aedc6a2b65
michalis-oik <mi8chal@gmail.com>
2025-08-26 19:06:17 +0300

    update bottom nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,24 +13,28 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.blue,
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.amber, // affects both icon & label when selected
-        unselectedItemColor: Colors.white, // affects both when unselected
-        selectedLabelStyle: const TextStyle(color: Colors.green),    // override label color when selected
-        unselectedLabelStyle: const TextStyle(color: Colors.red),    // override label color when unselected
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.brown[500], // affects both icon & label when selected
+        unselectedItemColor: Colors.grey[700], // affects both when unselected
+        //selectedLabelStyle: const TextStyle(color: Colors.green),    // override label color when selected
+        //unselectedLabelStyle: const TextStyle(color: Colors.red),    // override label color when unselected
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),    // override label color when selected
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
+            icon: Icon(Icons.favorite_border),
             label: 'Favorite',
-            backgroundColor: Colors.red,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded),
+            label: 'Profile',
           ),
         ],
       ),
