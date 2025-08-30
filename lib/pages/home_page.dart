17115e83e78b918f5dca58dd38446f985e0a1b3b
michalis-oik <mi8chal@gmail.com>
2025-08-30 12:34:09 +0300

    update donut app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,32 +11,143 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.grey[100],
-        selectedItemColor: Colors.brown[500], // affects both icon & label when selected
-        unselectedItemColor: Colors.grey[700], // affects both when unselected
-        //selectedLabelStyle: const TextStyle(color: Colors.green),    // override label color when selected
-        //unselectedLabelStyle: const TextStyle(color: Colors.red),    // override label color when unselected
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),    // override label color when selected
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 178, 122, 191), Color.fromARGB(255, 202, 161, 205)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            label: 'Favorite',
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                child: Column(
+                  children: [
+                    // Top row
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.menu_rounded,
+                          color: Colors.grey[800],
+                          size: 30,
+                        ),
+                        const Spacer(),
+                        // Shopping bag with badge
+                        Stack(
+                          children: [
+                            Icon(
+                              Icons.shopping_bag_outlined,
+                              color: Colors.grey[800],
+                              size: 30,
+                            ),
+                            Positioned(
+                              right: 0,
+                              bottom: 0,
+                              child: Container(
+                                //padding: EdgeInsets.all(2),
+                                decoration: BoxDecoration(
+                                  color: Colors.pink[300],
+                                  shape: BoxShape.circle,
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 16,
+                                  minHeight: 16,
+                                ),
+                                child: Text(
+                                  '2',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // Introduction Row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(width: 10),
+                        SizedBox(
+                          width: 250,
+                          child: Text(
+                            "We Offer Happiness In Circles",
+                            softWrap: true,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(255, 43, 43, 43),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ), 
+
+                    const SizedBox(height: 40),
+
+                    // Search Bar row
+                    Row(
+                      children: [
+                        //Spacer(),
+                        Container(
+                          width: 300,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.search,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                "Search for donut",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          width: 45,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.pink[300],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.filter_list,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ]
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag_outlined),
-            label: 'Shop',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline_rounded),
-            label: 'Profile',
-          ),
-        ],
+        ),
       ),
     );
   }
