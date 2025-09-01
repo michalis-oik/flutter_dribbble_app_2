e250903fea319b5679b4372189912ab7841cc7eb
michalis-oik <mi8chal@gmail.com>
2025-09-01 19:14:56 +0300

    update donut cards with icons on the bottom and animation on the heart when clicked

--- lib/utils/donut_card.dart
+++ lib/utils/donut_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 class DonutCard extends StatefulWidget {
   final String title;
@@ -22,9 +23,17 @@ class DonutCard extends StatefulWidget {
 
 class _DonutCardState extends State<DonutCard> {
   double _scale = 1.0;
+  bool _isFavorited = false; // Track favorite state
 
   void _onTapDown(_) => setState(() => _scale = 1.1);
   void _onTapUp(_) => setState(() => _scale = 1.0);
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorited = !_isFavorited;
+    });
+    print('Favorite ${_isFavorited ? 'added' : 'removed'} for ${widget.title}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +47,7 @@ class _DonutCardState extends State<DonutCard> {
         duration: const Duration(milliseconds: 150),
         curve: Curves.easeOut,
         child: Container(
-          margin: const EdgeInsets.all(10), // Space for scaling
+          margin: const EdgeInsets.all(10),
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(30),
@@ -50,34 +59,71 @@ class _DonutCardState extends State<DonutCard> {
               ),
             ],
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
             children: [
-              Icon(widget.icon, size: 50, color: Colors.pink[300]),
-              const SizedBox(height: 10),
-              Text(
-                widget.title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.black87,
+              // Main content
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(widget.icon, size: 50, color: Colors.pink[300]),
+                    const SizedBox(height: 10),
+                    Text(
+                      widget.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      widget.subtitle,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      widget.price,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.pink,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 5),
-              Text(
-                widget.subtitle,
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey,
+              
+              // Bottom left button - Favorite with toggle
+              Positioned(
+                left: 12,
+                bottom: 12,
+                child: GestureDetector(
+                  onTap: _toggleFavorite,
+                  behavior: HitTestBehavior.opaque,
+                  child: AnimatedCrossFade(
+                    duration: const Duration(milliseconds: 200),
+                    crossFadeState: _isFavorited ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                    firstChild: Icon(Icons.favorite_border, size: 22, color: Colors.grey[600]),
+                    secondChild: Icon(Icons.favorite, size: 22, color: Colors.pink[300]),
+                  ),
                 ),
               ),
-              const SizedBox(height: 10),
-              Text(
-                widget.price,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.pink,
+              
+              // Bottom right button - Add to cart
+              Positioned(
+                right: 12,
+                bottom: 12,
+                child: GestureDetector(
+                  onTap: () {
+                    print('Add to cart button pressed for ${widget.title}');
+                  },
+                  behavior: HitTestBehavior.opaque,
+                  child: Icon(Icons.add_shopping_cart, size: 22, color: Colors.pink[300]),
                 ),
               ),
             ],
